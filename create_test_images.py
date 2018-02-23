d1e7c1f1b7cb6182a42c510110c493ebb1c89c39
Yoann Benoit <ybenoit@xebia.fr>
2018-02-23 09:01:06 +0100

    Add scripts

---
+++ create_test_images.py
@@ -0,0 +1,15 @@
+import keras
+from keras.datasets import mnist
+import os
+import numpy as np
+import scipy.misc
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+if not os.path.exists("test_images"):
+    os.makedirs("test_images")
+
+for i in range(len(test_images)):
+    img = np.array(test_images[i]).reshape((28, 28))
+    label = test_labels[i]
+    scipy.misc.imsave("test_images/%s_%s.png" % (label, i), img)
